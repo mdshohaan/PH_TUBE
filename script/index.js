182c57b5d917e216c4ad724ea1bd25def58fd34b
mdshohaan <irfanulislamdu@gmail.com>
2024-10-03 22:30:34 +0600

    first commit

--- script/index.js
+++ script/index.js
@@ -1,3 +1,12 @@
+
+function getTime(time){
+    // get hour rest second
+    const hour = parseInt(time/3600)
+    let remainingSecond = time % 3600
+    const minute = parseInt(remainingSecond / 60)
+    remainingSecond = remainingSecond % 60
+    return `${hour} hour ${minute} minute  ${remainingSecond} second ago`
+}
 //* 1. fetch,load and show category
 // create loadCategory
 const loadCategory = () => {
@@ -15,6 +24,15 @@ const loadVideos = () => {
     .then((data) => displayvideos(data.videos))
     .catch((error) => console.log(error));
 };
+const loadCategoryVideos =(id) =>{
+    // alert(id)
+     // fetch data
+  fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
+  .then((res) => res.json())
+  .then((data) => displayvideos(data.category))
+  .catch((error) => console.log(error));
+
+}
 // const cardDemo = {
 //   category_id: "1001",
 //   video_id: "aaad",
@@ -37,6 +55,23 @@ const loadVideos = () => {
 
 const displayvideos = (videos) => {
   const videoContainer = document.getElementById("videos");
+  videoContainer.innerHTML =""
+
+if(videos.length == 0){
+    videoContainer.classList.remove('grid')
+     videoContainer.innerHTML =`
+     <div class = "min-h-[300px] w-full flex flex-col gap-5 justify-centern items-center">
+     <img src = "assets/Icon.png"/>
+     <h2 class = "text-center text-xl font-bold">
+     No Content Here in this Category
+     </h2>
+     </div>
+     `
+     return
+} else{
+    videoContainer.classList.add('grid')
+}
+
   videos.forEach((video) => {
     console.log(video);
     const card = document.createElement("div");
@@ -47,18 +82,29 @@ const displayvideos = (videos) => {
       src=${video.thumbnail}
       class ="h-full w-full object-cover"
       alt="Shoes" />
-      <span class="absolute right-2 bottom-2 bg-black rounded p-1 text-white">${video.others.posted_date}</span>
+      ${
+        video.others.posted_date?.length == 0
+          ? ""
+          : `<span class="absolute right-2 text-xs bottom-2 bg-black rounded p-1 text-white">${getTime(video.others.posted_date)}</span>`
+      }
+      
   </figure>
   <div class="px-0 py-2 flex gap-2">
    <div>
-   <img class="w-10 h-10 rounded-full object-cover" src =${video.authors[0].profile_picture}  />
+   <img class="w-10 h-10 rounded-full object-cover" src =${
+     video.authors[0].profile_picture
+   }  />
    </div>
    <div>
    <h2 class = "font-bold">${video.title} </h2>
      <div class ="flex gap-2 items-center">  
         <p class="text-gray-400">${video.authors[0].profile_name}</p>
 
-        ${video.authors[0].verified === true ? ` <img class="w-5" src = "https://img.icons8.com/?size=96&id=D9RtvkuOe31p&format=png"/>` : ""}
+        ${
+          video.authors[0].verified === true
+            ? ` <img class="w-5" src = "https://img.icons8.com/?size=96&id=D9RtvkuOe31p&format=png"/>`
+            : ""
+        }
        
      </div>
    <p></p>
@@ -77,12 +123,15 @@ const displayCategories = (categories) => {
   categories.forEach((item) => {
     console.log(item);
     // create btn
-    const button = document.createElement("button");
-    button.classList = "btn";
-    button.innerText = item.category;
+    const buttonContainer = document.createElement("div");
+   buttonContainer.innerHTML = `
+   <button onclick ="loadCategoryVideos(${item.category_id})" class="btn">
+   ${item.category}
+   </button
+   `
 
     // add btn to category container
-    categoryContainer.append(button);
+    categoryContainer.append(buttonContainer);
   });
 };
 loadCategory();
--- script/text.js
+++ script/text.js
@@ -1,2 +1,12 @@
 
-console.log(`${isVerified=== true ? "user if verified":'user is not verified'}`);
+// console.log(`${isVerified=== true ? "user if verified":'user is not verified'}`);
+
+function getTime(time){
+    // get hour rest second
+    const hour = parseInt(time/3600)
+    let remainingSecond = time % 3600
+    const minute = parseInt(remainingSecond / 60)
+    remainingSecond = remainingSecond % 60
+    return `${hour} hour ${minute} minute ago ${remainingSecond} second ago`
+}
+console.log(getTime(4320));
